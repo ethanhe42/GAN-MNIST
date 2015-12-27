Fixed Bug in Batch Normalization

lsun/model.py
@@ -60,39 +60,39 @@ class DCGAN():
         self.dim_W4 = dim_W4
         self.dim_W5 = dim_W5
 
-        self.gen_W1 = tf.Variable(tf.random_normal([dim_z, dim_W1*4*4], stddev=0.02), name='gen_W1')
-        self.gen_bn_g1 = tf.Variable( tf.random_normal([dim_W1*4*4], mean=1.0, stddev=0.02), name='gen_bn_g1')
+        self.gen_W1 = tf.Variable(tf.truncated_normal([dim_z, dim_W1*4*4], stddev=0.02), name='gen_W1')
+        self.gen_bn_g1 = tf.Variable( tf.truncated_normal([dim_W1*4*4], mean=1.0, stddev=0.02), name='gen_bn_g1')
         self.gen_bn_b1 = tf.Variable( tf.zeros([dim_W1*4*4]), name='gen_bn_b1')
 
-        self.gen_W2 = tf.Variable(tf.random_normal([5,5,dim_W2, dim_W1], stddev=0.02), name='gen_W2')
-        self.gen_bn_g2 = tf.Variable( tf.random_normal([dim_W2], mean=1.0, stddev=0.02), name='gen_bn_g2')
+        self.gen_W2 = tf.Variable(tf.truncated_normal([5,5,dim_W2, dim_W1], stddev=0.02), name='gen_W2')
+        self.gen_bn_g2 = tf.Variable( tf.truncated_normal([dim_W2], mean=1.0, stddev=0.02), name='gen_bn_g2')
         self.gen_bn_b2 = tf.Variable( tf.zeros([dim_W2]), name='gen_bn_b2')
 
-        self.gen_W3 = tf.Variable(tf.random_normal([5,5,dim_W3, dim_W2], stddev=0.02), name='gen_W3')
-        self.gen_bn_g3 = tf.Variable( tf.random_normal([dim_W3], mean=1.0, stddev=0.02), name='gen_bn_g3')
+        self.gen_W3 = tf.Variable(tf.truncated_normal([5,5,dim_W3, dim_W2], stddev=0.02), name='gen_W3')
+        self.gen_bn_g3 = tf.Variable( tf.truncated_normal([dim_W3], mean=1.0, stddev=0.02), name='gen_bn_g3')
         self.gen_bn_b3 = tf.Variable( tf.zeros([dim_W3]), name='gen_bn_b3')
 
-        self.gen_W4 = tf.Variable(tf.random_normal([5,5,dim_W4, dim_W3], stddev=0.02), name='gen_W4')
-        self.gen_bn_g4 = tf.Variable( tf.random_normal([dim_W4], mean=1.0, stddev=0.02), name='gen_bn_g4')
+        self.gen_W4 = tf.Variable(tf.truncated_normal([5,5,dim_W4, dim_W3], stddev=0.02), name='gen_W4')
+        self.gen_bn_g4 = tf.Variable( tf.truncated_normal([dim_W4], mean=1.0, stddev=0.02), name='gen_bn_g4')
         self.gen_bn_b4 = tf.Variable( tf.zeros([dim_W4]), name='gen_bn_b4')
 
-        self.gen_W5 = tf.Variable(tf.random_normal([5,5,dim_W5, dim_W4], stddev=0.02), name='gen_W5')
+        self.gen_W5 = tf.Variable(tf.truncated_normal([5,5,dim_W5, dim_W4], stddev=0.02), name='gen_W5')
 
-        self.discrim_W1 = tf.Variable(tf.random_normal([5,5,dim_W5,dim_W4], stddev=0.02), name='discrim_W1')
+        self.discrim_W1 = tf.Variable(tf.truncated_normal([5,5,dim_W5,dim_W4], stddev=0.02), name='discrim_W1')
 
-        self.discrim_W2 = tf.Variable(tf.random_normal([5,5,dim_W4,dim_W3], stddev=0.02), name='discrim_W2')
-        self.discrim_bn_g2 = tf.Variable( tf.random_normal([dim_W3], mean=1.0, stddev=0.02), name='discrim_bn_g2')
+        self.discrim_W2 = tf.Variable(tf.truncated_normal([5,5,dim_W4,dim_W3], stddev=0.02), name='discrim_W2')
+        self.discrim_bn_g2 = tf.Variable( tf.truncated_normal([dim_W3], mean=1.0, stddev=0.02), name='discrim_bn_g2')
         self.discrim_bn_b2 = tf.Variable( tf.zeros([dim_W3]), name='discrim_bn_b2')
 
-        self.discrim_W3 = tf.Variable(tf.random_normal([5,5,dim_W3,dim_W2], stddev=0.02), name='discrim_W3')
-        self.discrim_bn_g3 = tf.Variable( tf.random_normal([dim_W2], mean=1.0, stddev=0.02), name='discrim_bn_g3')
+        self.discrim_W3 = tf.Variable(tf.truncated_normal([5,5,dim_W3,dim_W2], stddev=0.02), name='discrim_W3')
+        self.discrim_bn_g3 = tf.Variable( tf.truncated_normal([dim_W2], mean=1.0, stddev=0.02), name='discrim_bn_g3')
         self.discrim_bn_b3 = tf.Variable( tf.zeros([dim_W2]), name='discrim_bn_b3')
 
-        self.discrim_W4 = tf.Variable(tf.random_normal([5,5,dim_W2,dim_W1], stddev=0.02), name='discrim_W4')
-        self.discrim_bn_g4 = tf.Variable( tf.random_normal([dim_W1], mean=1.0, stddev=0.02), name='discrim_bn_g4')
+        self.discrim_W4 = tf.Variable(tf.truncated_normal([5,5,dim_W2,dim_W1], stddev=0.02), name='discrim_W4')
+        self.discrim_bn_g4 = tf.Variable( tf.truncated_normal([dim_W1], mean=1.0, stddev=0.02), name='discrim_bn_g4')
         self.discrim_bn_b4 = tf.Variable( tf.zeros([dim_W1]), name='discrim_bn_b4')
 
-        self.discrim_W5 = tf.Variable(tf.random_normal([4*4*dim_W1,1], stddev=0.02), name='discrim_W5')
+        self.discrim_W5 = tf.Variable(tf.truncated_normal([4*4*dim_W1,1], stddev=0.02), name='discrim_W5')
 
         self.gen_params = [
                 self.gen_W1, self.gen_bn_g1, self.gen_bn_b1,

lsun/train.py
@@ -2,6 +2,8 @@ import ipdb
 import os
 import pandas as pd
 import numpy as np
+import lmdb
+from glob import glob
 from model import *
 from util import *
 
@@ -10,17 +12,20 @@ learning_rate = 0.0002
 batch_size = 128
 image_shape = [64,64,3]
 dim_z = 100
-dim_W1 = 1024
-dim_W2 = 512
-dim_W3 = 256
-dim_W4 = 128
+dim_W1 = 512#1024
+dim_W2 = 256#512
+dim_W3 = 128#256
+dim_W4 = 64#128
 dim_W5 = 3
 
 visualize_dim=196
 
-face_image_path = '/media/storage3/Study/data/celeb/img_align_celeba'
-face_images = filter(lambda x: x.endswith('jpg'), os.listdir(face_image_path))
+lsun_image_path = '/media/storage3/Study/data/lsun/images/0'
+lsun_images = []
+for dir,_,_ in os.walk(lsun_image_path):
+    lsun_images.append(glob(os.path.join(dir, '*.jpg')))
 
+lsun_images = lsun_images[0]
 dcgan_model = DCGAN(
         batch_size=batch_size,
         image_shape=image_shape,
@@ -51,29 +56,27 @@ iterations = 0
 k = 2
 
 for epoch in range(n_epochs):
-    np.random.shuffle(face_images)
 
     for start, end in zip(
-            range(0, len(face_images), batch_size),
-            range(batch_size, len(face_images), batch_size)
+            range(0, len(lsun_images), batch_size),
+            range(batch_size, len(lsun_images), batch_size)
             ):
 
-        batch_image_files = face_images[start:end]
-        batch_images = map(lambda x: crop_resize( os.path.join( face_image_path, x) ), batch_image_files)
+        batch_image_files = lsun_images[start:end]
+        batch_images = map(lambda x: crop_resize( os.path.join( lsun_image_path, x) ), batch_image_files)
         batch_images = np.array(batch_images).astype(np.float32)
         batch_z = np.random.uniform(-1, 1, size=[batch_size, dim_z]).astype(np.float32)
 
-        if np.mod( iterations, k ) == 0:
+        p_real_val, p_gen_val, h_real_val, h_gen_val = sess.run([p_real, p_gen, h_real, h_gen], feed_dict={Z_tf:batch_z, image_tf:batch_images})
+        if np.mod( iterations, k ) != 0:
             _, gen_loss_val = sess.run(
                     [train_op_gen, g_cost_tf],
                     feed_dict={
                         Z_tf:batch_z,
                         })
-            discrim_loss_val, p_real_val, p_gen_val, h_real_val, h_gen_val = sess.run([d_cost_tf,p_real,p_gen, h_real, h_gen], feed_dict={Z_tf:batch_z, image_tf:batch_images})
             print "=========== updating G =========="
             print "iteration:", iterations
             print "gen loss:", gen_loss_val
-            print "discrim loss:", discrim_loss_val
 
         else:
             _, discrim_loss_val = sess.run(
@@ -82,13 +85,11 @@ for epoch in range(n_epochs):
                         Z_tf:batch_z,
                         image_tf:batch_images
                         })
-            gen_loss_val, p_real_val, p_gen_val, h_real_val, h_gen_val = sess.run([g_cost_tf, p_real, p_gen, h_real, h_gen], feed_dict={Z_tf:batch_z, image_tf:batch_images})
             print "=========== updating D =========="
             print "iteration:", iterations
-            print "gen loss:", gen_loss_val
             print "discrim loss:", discrim_loss_val
 
-        ipdb.set_trace()
+        print "real h:", h_real_val.mean(),"  gen h:", h_gen_val.mean()
 
         if np.mod(iterations, 100) == 0:
             generated_samples = sess.run(

mnist/model.py
@@ -6,7 +6,7 @@ def batchnormalize(X, eps=1e-8, g=None, b=None):
     if X.get_shape().ndims == 4:
         mean = tf.reduce_mean(X, [0,1,2])
         std = tf.reduce_mean( tf.square(X-mean), [0,1,2] )
-        X = (X-mean) / std
+        X = (X-mean) / tf.sqrt(std+eps)
 
         if g is not None and b is not None:
             g = tf.reshape(g, [1,1,1,-1])
@@ -16,7 +16,7 @@ def batchnormalize(X, eps=1e-8, g=None, b=None):
     elif X.get_shape().ndims == 2:
         mean = tf.reduce_mean(X, 0)
         std = tf.reduce_mean(tf.square(X-mean), 0)
-        X = (X-mean) / std
+        X = (X-mean) / tf.sqrt(std+eps)
 
         if g is not None and b is not None:
             g = tf.reshape(g, [1,-1])